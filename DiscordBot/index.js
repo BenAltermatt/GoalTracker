cb316f9c80466f4e5a9e958be50cc8f42efce6df
BenAltermatt <ben.altermatt20@gmail.com>
2022-05-20 11:38:22 -0400

    Added social credit queries

--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -1,6 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const Discord = require('discord.js');
 const creds = require('./credentials.json');
+const { MongoClient } = require('mongodb');
+const dbclient = new MongoClient(creds.MongoConnection);
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -11,18 +14,23 @@ const client = new Discord.Client({
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
-client.on('messageCreate', async (message) => {
+// LISTENERS
+client.on('messageCreate', handleMessage);
+// NEW MESSAGE LISTENER
+async function handleMessage(message) {
     let content = message.content;
+    let user = message.member.user;
+    await awardPoints(10, user);
     // er joke
-    if (content.length >= 2 && (content.substring(content.length - 2) == 'er')) {
-        await message.reply(content + `? I hardly know her!`);
+    if (content.length >= 2 && (content.substring(content.length - 2) === 'er')) {
+        if (user.tag == 'Gizmoda#9097')
+            await message.reply(content + `? I hardly know her!`);
     }
     if (content.substring(0, 1) !== '>')
         return;
     content = content.substring(1);
     if (content == 'ping') {
         let user = message.member.toString();
-        console.log(user);
         await message.reply(`${user}, Pong!!!`);
     }
     if (content == 'cum') {
@@ -34,5 +42,56 @@ client.on('messageCreate', async (message) => {
     if (content == 'squirt') {
         await message.reply(`please get help.`);
     }
-});
+    if (content.substring(0, 2) == 'sc') {
+        let mentions = message.mentions;
+        mentions.members.forEach(async (val) => {
+            await reportPoints(val.user, message);
+        });
+    }
+}
+// this will report the number of points that a user
+// has. If they have not been found in the database, they
+// are told that the user has not been registered within the
+// social credit system
+async function reportPoints(user, queryMessage) {
+    await dbclient.connect();
+    const collection = await dbclient.db(creds.DatabaseName).collection(creds.UserColName);
+    // find out if the user is registered
+    let query = await collection.findOne({ tag: user.tag });
+    console.log(query);
+    await dbclient.close();
+    if (query === null) // not registered
+     {
+        await queryMessage.reply(`${user.tag} is not formally registered in the social credit system 
+                            and therefore does not legally exist!\nTo be registered, send a 
+                            message in any server with MonkeBot in it!`);
+    }
+    else // registered
+     {
+        // we can manage our responses based on how good the credit is
+        let credVal = query.credit;
+        // great credit
+        if (credVal > 10000) {
+            await queryMessage.reply(`${user.tag} has great social credit at ${credVal} points! What a good citizen!!`);
+        }
+        else if (credVal > 0) // good credit
+         {
+            await queryMessage.reply(`${user.tag} has okay social credit. Not the best, but ${credVal} isn't anything to sneeze at!`);
+        }
+        else // bad credit
+         {
+            await queryMessage.reply(`${user.tag} is in danger of becoming an unperson! Very bad credit!!! ${credVal} points.`);
+        }
+    }
+}
+// this will take a user and update their social credit
+// score by the passed number of points
+async function awardPoints(numPoints, user) {
+    await dbclient.connect();
+    console.log('Successful database connection');
+    const db = await dbclient.db(creds.DatabaseName);
+    const collection = await db.collection(creds.UserColName);
+    console.log(await collection.updateOne({ tag: user.tag }, { $inc: { credit: numPoints } }, { upsert: true }));
+    dbclient.close();
+}
 client.login(creds.token);
--- DiscordBot/index.tsx
+++ DiscordBot/index.tsx
@@ -1,5 +1,10 @@
+import { Message, User} from "discord.js";
+
 const Discord = require('discord.js');
 const creds = require('./credentials.json');
+const {MongoClient} = require('mongodb');
+const dbclient = new MongoClient(creds.MongoConnection);
+
 
 const client = new Discord.Client({
     intents : [
@@ -13,12 +18,20 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on('messageCreate',  async (message : any) => {
+// LISTENERS
+client.on('messageCreate',  handleMessage);
+
+// NEW MESSAGE LISTENER
+async function handleMessage(message : any) {
     let content : string = message.content;
+    let user : User = message.member.user;
+
+    await awardPoints(10, user);
 
     // er joke
-    if (content.length >= 2 && (content.substring(content.length - 2) == 'er')) {
-        await message.reply(content + `? I hardly know her!`);
+    if (content.length >= 2 && (content.substring(content.length - 2) === 'er')) {
+        if(user.tag == 'Gizmoda#9097')
+            await message.reply(content + `? I hardly know her!`);
     }
 
     if(content.substring(0,1) !== '>') return;
@@ -27,7 +40,6 @@ client.on('messageCreate',  async (message : any) => {
 
     if (content == 'ping') {
         let user = message.member.toString();
-        console.log(user);
         await message.reply(`${user}, Pong!!!`);
     }
 
@@ -42,8 +54,65 @@ client.on('messageCreate',  async (message : any) => {
     if (content == 'squirt') {
         await message.reply(`please get help.`);
     }
-})
 
+    if (content.substring(0, 2) == 'sc') {
+       let mentions = message.mentions;
+       mentions.members.forEach(async (val : any) => {
+            await reportPoints(val.user, message);
+       })
+    }
+}
+
+// this will report the number of points that a user
+// has. If they have not been found in the database, they
+// are told that the user has not been registered within the
+// social credit system
+async function reportPoints(user: User, queryMessage: Message) {
+    await dbclient.connect();
+    const collection = await dbclient.db(creds.DatabaseName).collection(creds.UserColName);
+
+    // find out if the user is registered
+    let query = await collection.findOne({tag : user.tag});
+    console.log(query);
+    await dbclient.close();
+
+    if(query === null) // not registered
+    {
+        await queryMessage.reply(`${user.tag} is not formally registered in the social credit system 
+                            and therefore does not legally exist!\nTo be registered, send a 
+                            message in any server with MonkeBot in it!`);
+    }
+    else // registered
+    {
+        // we can manage our responses based on how good the credit is
+        let credVal = query.credit;
+        
+        // great credit
+        if(credVal > 10000)
+        {
+            await queryMessage.reply(`${user.tag} has great social credit at ${credVal} points! What a good citizen!!`);
+        } else if(credVal > 0) // good credit
+        {
+            await queryMessage.reply(`${user.tag} has okay social credit. Not the best, but ${credVal} isn't anything to sneeze at!`);
+        } else // bad credit
+        {
+            await queryMessage.reply(`${user.tag} is in danger of becoming an unperson! Very bad credit!!! ${credVal} points.`);
+        }
+    }
+}
+
+
+// this will take a user and update their social credit
+// score by the passed number of points
+async function awardPoints(numPoints : number, user: User) {
+    await dbclient.connect();
+    console.log('Successful database connection');
+    const db = await dbclient.db(creds.DatabaseName);
+    const collection = await db.collection(creds.UserColName);
+
+    console.log(await collection.updateOne({tag: user.tag} , {$inc : {credit: numPoints}}, {upsert : true}));
+    dbclient.close();
+}
 
 
 client.login(creds.token);
